943aa8a362ff0966ed52e7a5dd3a75f1e0f1c5ac
Jennifer Lipps <jjlipps@gmail.com>
2019-07-13 13:27:38 -0500

    footer, project cards

--- src/javascripts/components/cards/cards.js
+++ src/javascripts/components/cards/cards.js
@@ -1,28 +1,31 @@
 import 'bootstrap';
 import util from '../../helpers/util';
 import projectsInfo from '../../helpers/data/projectsData';
+import './cards.scss';
 
-const createProjectCards = (projects) => {
-  let domString = '';
-  projects.forEach((project) => {
-    domString += '<div class="card">';
-    domString += `<h2>${project.title}</h2>`;
-    domString += `<p>${project.screenshot}</p>`;
-    domString += `<p>${project.description}</p>`;
-    domString += `<p>${project.technologiesUsed}</p>`;
-    domString += `<p>View project <a href=${project.url}>HERE</a></p>`;
-    domString += `<p><a href=${project.githubUrl}>GitHub</a></p>`;
-    domString += '</div>';
-  });
-  util.printToDom('projectsPage', domString);
-};
-
-const displayProjects = () => {
+const createProjectCards = () => {
   projectsInfo.getProjectsData()
     .then((projects) => {
-      createProjectCards(projects);
+      console.error(projects);
+      let domString = '';
+      for (let i = 0; i < projects.length; i += 1) {
+        console.error(projects[i].available);
+        if (projects[i].available === true) {
+          domString += '<div class="projects col-3">';
+          domString += '<div class="card">';
+          domString += `<h2>${projects[i].title}</h2>`;
+          // domString += `<div id="projectImg">${projects[i].screenshot}</div>`;
+          domString += `<p>${projects[i].description}</p>`;
+          domString += `<p>${projects[i].technologiesUsed}</p>`;
+          domString += `<p>View project <a href=${projects[i].url}>HERE</a></p>`;
+          domString += `<a href=${projects[i].githubUrl}>GitHub</a>`;
+          domString += '</div>';
+          domString += '</div>';
+        }
+      }
+      util.printToDom('projectsPage', domString);
     })
-    .catch(err => console.error('no projects', err));
+    .catch(err => console.error('no projects from createCards', err));
 };
 
-export default { displayProjects };
+export default { createProjectCards };
